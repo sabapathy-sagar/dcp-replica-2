7baf644f319496a76dd2740d66af5adbe0ac6353
sabapathy-sagar <33907389+sabapathy-sagar@users.noreply.github.com>
2018-05-10 22:02:32 +0200

    style for login button

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default props => {
   return (
-    <button onClick={props.authCb}>
-      {props.isAuthenticated ? "LogOut" : "Login"}
+    <button class="button is-text has-text-link" onClick={props.authCb}>
+      {props.isAuthenticated ? "Log Out" : "Log In"}
     </button>
   );
 };
