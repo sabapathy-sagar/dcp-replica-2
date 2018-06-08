8a0315bc391386d4e5236e20b8bab1e780b1c7e6
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2018-06-08 15:50:30 +0200

    feat(convert home component to functional component)

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,28 +3,19 @@ import Welcome from "../Welcome/Welcome";
 import LoginAds from "../Login/LoginAds";
 import { Link } from "react-router-dom";
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div class="hero-body has-background-grey-lighter">
-        <div class="container has-text-centered">
-          <div class="title">
-            <Welcome name="DCP" />
-          </div>
-          <h2 class="subtitle">
-            Take a look at our products:{" "}
-            <Link to="/products">
-              <button class="button is-primary is-rounded">
-                Go To Products
-              </button>
-            </Link>
-            <LoginAds text="Log on to our website and get exclusive discounts" />
-          </h2>
-        </div>
+export default () => (
+  <div class="hero-body has-background-grey-lighter">
+    <div class="container has-text-centered">
+      <div class="title">
+        <Welcome name="DCP" />
       </div>
-    );
-  }
-}
+      <h2 class="subtitle">
+        Take a look at our products:{" "}
+        <Link to="/products">
+          <button class="button is-primary is-rounded">Go To Products</button>
+        </Link>
+        <LoginAds text="Log on to our website and get exclusive discounts" />
+      </h2>
+    </div>
+  </div>
+);
